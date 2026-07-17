624ddc18ab0105b2a188f19d5a79746b568f3952
Mir-Abbas-creator <abuazizchen@gmail.com>
2026-07-17 14:08:47 +0530

    Create Day 3-HW 29 Valid Parenthis.java

---
+++ Day 3-HW 29 Valid Parenthis.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}

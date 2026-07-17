e1cc93d02e67b30e019694881ca0a4cbaae99731
Mir-Abbas-creator <abuazizchen@gmail.com>
2026-07-17 14:56:02 +0530

    Create Day 5-HW 65 Valid Number.java

---
+++ Day 5-HW 65 Valid Number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean seenNum = false;
+        boolean seenDot = false;
+        boolean seenE = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                seenNum = true;
+            } else if (c == '.') {
+                if (seenDot || seenE) return false;
+                seenDot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (seenE || !seenNum) return false;
+                seenE = true;
+                seenNum = false;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+            } else {
+                return false;
+            }
+        }
+        
+        return seenNum;
+    }
+}

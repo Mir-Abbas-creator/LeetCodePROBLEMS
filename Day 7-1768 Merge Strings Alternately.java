7182ebf68cc3c367ea814d41d82e33bab8942727
Mir-Abbas-creator <abuazizchen@gmail.com>
2026-07-21 11:09:25 +0530

    Update Day 7-1768 Merge Strings Alternately.java

--- Day 7-1768 Merge Strings Alternately.java
+++ Day 7-1768 Merge Strings Alternately.java
@@ -1,17 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < m || i < n; ++i) {
-            if (i < m) {
-                ans.append(word1.charAt(i));
-            }
-            if (i < n) {
-                ans.append(word2.charAt(i));
-            }
+        char[] a = word1.toCharArray();
+        char[] b = word2.toCharArray();
+
+        StringBuilder sb = new StringBuilder(a.length + b.length);
+
+        int min = Math.min(a.length, b.length);
+
+        for (int i = 0; i < min; i++) {
+            sb.append(a[i]).append(b[i]);
         }
-        
-        return ans.toString();
+
+        if (a.length > min) {
+            sb.append(a, min, a.length - min);
+        } else if (b.length > min) {
+            sb.append(b, min, b.length - min);
+        }
+
+        return sb.toString();
     }
 }
